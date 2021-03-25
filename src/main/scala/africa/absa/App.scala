448549598bfe6819e5579cac04f30111bdfa1281
Dennis Chu <dennis.chu@absa.africa>
2021-03-25 16:50:29 +0200

    added local binding option

--- src/main/scala/africa/absa/App.scala
+++ src/main/scala/africa/absa/App.scala
@@ -1,24 +1,30 @@
 package africa.absa
 
 /**
- * @author ${user.name}
- */
+  * @author ${user.name}
+  */
+
 import africa.absa.utils.ConfigFileManager
 import org.apache.spark.sql.SparkSession
 
 object App {
-  
 
-  def main(args : Array[String]) {
+
+  def main(args: Array[String]) {
 
     //config test
     println(ConfigFileManager.getConfig("my.config"))
 
-    val spark = SparkSession.builder().master("local").getOrCreate()
+    val spark = SparkSession.builder()
+      //remove to run on cluster
+      .master("local")
+      //i needed this as zscaler is messing with my ip routing
+      .config("spark.driver.bindAddress", "127.0.0.1")
+      .getOrCreate()
 
     import spark.implicits._
 
-    val df = spark.sparkContext.parallelize(Seq((1,2,3))).toDF
+    val df = spark.sparkContext.parallelize(Seq((1, 2, 3))).toDF
 
     df.show
 
